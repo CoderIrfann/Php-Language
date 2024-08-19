0ece2a1300347b348fe5d260edabe4900b83d60e
Coder Irfan <mohdirfan70097@gmail.com>
2024-08-20 01:17:27 +0530

    Serching what is echo is it insensitive or not

--- Starting.php
+++ Starting.php
@@ -2,3 +2,17 @@
 echo "Hello Php \n";
 echo 2+2;
 ?>
+
+What is echo 
+echo is a language construct(Keyword) that is used to display the code in browser
+
+how it is case insensitive
+
+Case Insensitive means if user write the code in lowerCase still the output will display in browser, Similarly if user wrirte the code in Uppercase then Also the the output will display means in php fully user choise how it written the code 
+example :
+<?php 
+echo 2+2;  // it will also run
+ECHO 2+2; // it will also
+eChO 2+2; // it will also
+?>
+
